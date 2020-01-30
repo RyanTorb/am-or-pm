65df26c629450a7dbd6160d60acf9bb5be0b0a71
Ryan Torbic <ryan.torbic@lumiscorp.com>
2020-01-30 11:58:22 -0500

    d;flk Please enter the commit message for your changes. Lines starting

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 
 function postMessage() {
   var botResponse, options, body, botReq;
-  botResponse = "AM or PM?;
+  botResponse = "AM or PM?";
 
   options = {
     hostname: 'api.groupme.com',
